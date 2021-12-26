16ac6d0d52bbb63b84e04e1223e867dbd973122b
Bruno Peralva <brunoperalva@hotmail.com>
2021-12-26 11:51:27 -0300

    Correção de variavel

--- utils/getMessageText.js
+++ utils/getMessageText.js
@@ -1,15 +1,15 @@
 const capitalize = require("./capitalize");
 const getLanguage = require("./getLanguage");
 
-module.exports = (language_code, data, urlList) => {
-    return(`<b>${capitalize(getLanguage(language_code, 'city'))}:</b> ${data.city}
-<b>Portal:</b> ${data.portal.name} (<a href="https://intel.ingress.com/intel?pll=${data.portal.coordinates.latitude},${data.portal.coordinates.longitude}">Intel</a>) (<a href="https://www.google.com/maps/@${data.portal.coordinates.latitude},${data.portal.coordinates.longitude},15z">Google Maps</a>)
-<b>${capitalize(getLanguage(language_code, 'distance'))}:</b> ${data.portal.distance.toFixed(3).replace('.', ',')} KM
-<b>${capitalize(getLanguage(language_code, 'language'))}:</b> ${data.language}
-<b>${capitalize(getLanguage(language_code, 'time'))}:</b> ${data.time} (${data.timeZone})
-${data.eventChannel != '' ? `<b>${capitalize(getLanguage(language_code, 'eventChannel'))}:</b> ${data.eventChannel}` : ''}
+module.exports = (languageCode, data, urlList) => {
+    return(`<b>${capitalize(getLanguage(languageCode, 'city'))}:</b> ${data.city}
+<b>Portal:</b> ${data.portal.name} (<a href="${data.portal.link}">Intel</a>) (<a href="https://www.google.com/maps/@${data.portal.coordinates.latitude},${data.portal.coordinates.longitude},15z">Google Maps</a>)
+<b>${capitalize(getLanguage(languageCode, 'distance'))}:</b> ${data.portal.distance.toFixed(3).replace('.', ',')} KM
+<b>${capitalize(getLanguage(languageCode, 'language'))}:</b> ${data.language}
+<b>${capitalize(getLanguage(languageCode, 'time'))}:</b> ${data.time} (${data.timeZone})
+${data.eventChannel != '' ? `<b>${capitalize(getLanguage(languageCode, 'eventChannel'))}:</b> ${data.eventChannel}` : ''}
 
-<b>${capitalize(getLanguage(language_code, 'eventDetails'))}:</b> <a href="${data.eventLink}">Link</a>
+<b>${capitalize(getLanguage(languageCode, 'eventDetails'))}:</b> <a href="${data.eventLink}">Link</a>
 
-<b>${capitalize(getLanguage(language_code, 'source'))}:</b> ${urlList}`);
+<b>${capitalize(getLanguage(languageCode, 'source'))}:</b> ${urlList}`);
 }
